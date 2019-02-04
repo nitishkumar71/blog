b22ba9d27bbe39cdba2f27719f37097bcba88467
Your Name <nitishkumar.singh@quantiphi.com>
2019-02-04 18:39:43 +0530

    initial commit

---
+++ gateway/springcloudgatewaytest/src/main/java/com/blog/springcloudgateway/springcloudgatewaytest/GatewayDiscoveryConfiguration.java
@@ -0,0 +1,60 @@
+package com.blog.springcloudgateway.springcloudgatewaytest;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+import reactor.core.publisher.Mono;
+
+@Configuration
+@EnableDiscoveryClient
+public class GatewayDiscoveryConfiguration {
+
+    @Bean
+    @ConditionalOnBean(DiscoveryClient.class)
+    @ConditionalOnProperty(name = "spring.cloud.gateway.discovery.locator.enabled")
+    public DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(DiscoveryClient discoveryClient,
+            DiscoveryLocatorProperties properties) {
+        return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
+    }
+
+    @Bean
+    @Order(-1)
+    public GlobalFilter a() {
+        return (exchange, chain) -> {
+            System.out.println("first pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                System.out.println("third post filter");
+            }));
+        };
+    }
+
+    @Bean
+    @Order(0)
+    public GlobalFilter b() {
+        return (exchange, chain) -> {
+            System.out.println("second pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                System.out.println("second post filter");
+            }));
+        };
+    }
+
+    @Bean
+    @Order(1)
+    public GlobalFilter c() {
+        return (exchange, chain) -> {
+            System.out.println("third pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                System.out.println("first post filter");
+            }));
+        };
+    }
+}
---
+++ gateway/zuul2/src/main/java/com/blog/zuul2test/zuul2/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.blog.zuul2test.zuul2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
