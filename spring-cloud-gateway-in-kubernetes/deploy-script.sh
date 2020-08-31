8bc082692067ec2a9ce0220115cffe335a5ed409
Nitishkumar Singh <nitishkumarsingh71@gmail.com>
2020-08-31 12:45:44 +0530

    updated from ribbon to spring loadbalancer

--- spring-cloud-gateway-in-kubernetes/deploy-script.sh
+++ spring-cloud-gateway-in-kubernetes/deploy-script.sh
@@ -5,6 +5,12 @@ docker build --tag=gateway:latest gateway/.
 docker build --tag=booking:latest booking/.
 docker build --tag=tracking:latest tracking/.
 
+# https://kind.sigs.k8s.io/docs/user/quick-start/
+# if you are using kind k8 cluster
+# kind load docker-image gateway:latest --name gateway
+# kind load docker-image booking:latest --name gateway
+# kind load docker-image tracking:latest --name gateway
+
 # create role and service account
 kubectl apply -f gateway/service-account.yaml
 kubectl apply -f gateway/namespace-role.yaml
